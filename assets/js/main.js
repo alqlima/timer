9cbb0684b46ad37381ab1fb91dc8522a02ced64d
André Queiroz <alqlima@gmail.com>
2020-10-30 22:24:06 -0300

    Starting timer construction

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const clock = document.querySelector('.clock');
+const start = document.querySelector('.start');
+const pause = document.querySelector('.pause');
+const reset = document.querySelector('.reset');
