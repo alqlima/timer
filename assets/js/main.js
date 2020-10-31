3ee8f3f30b6aba52ad495ad4cd58ccc5e91f9cdb
André Queiroz <alqlima@gmail.com>
2020-10-31 10:06:46 -0300

    ending timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,40 @@
-const clock = document.querySelector('.clock');
-const start = document.querySelector('.start');
-const pause = document.querySelector('.pause');
-const reset = document.querySelector('.reset');
+function clock() {
+    function getTimeFromSeconds(seconds) {
+        const date = new Date(seconds * 1000);
+        return date.toLocaleTimeString('pt-BR', {
+            timeZone: 'UTC'
+        });
+    }
+
+    const clock = document.querySelector('.clock');
+    let seconds = 0;
+    let timer;
+
+    function startWatch() {
+        timer = setInterval(function () {
+            seconds++;
+            clock.innerHTML = getTimeFromSeconds(seconds);
+        }, 1000)
+    }
+
+    document.addEventListener('click', function (e) {
+        const elements = e.target;
+
+        if (elements.classList.contains('start')) {
+            clock.classList.remove('paused');
+            clearInterval(timer);
+            startWatch();
+        }
+        if (elements.classList.contains('pause')) {
+            clearInterval(timer);
+            clock.classList.add('paused');
+        }
+        if (elements.classList.contains('reset')) {
+            clearInterval(timer);
+            clock.innerHTML = '00:00:00';
+            clock.classList.remove('paused');
+            seconds = 0;
+        }
+    });
+}
+clock();
